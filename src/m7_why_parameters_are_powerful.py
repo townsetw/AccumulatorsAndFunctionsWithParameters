53e72f0b3e9375a544cfeb9f20650563b33665c0
townsetw <townsetw@rose-hulman.edu>
2017-12-03 22:50:44 -0500

    completed m7

--- src/m7_why_parameters_are_powerful.py
+++ src/m7_why_parameters_are_powerful.py
@@ -2,18 +2,18 @@
 This module lets you experience the POWER of FUNCTIONS and PARAMETERS.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and TYLER TOWNSEND.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
 
 def main():
     """ Calls the   TEST   functions in this module. """
-    run_test_draw_circles()
+    #run_test_draw_circles()
     # Un-comment the next lines when you are ready to use them.
     # run_test_better_draw_circles()
-    # run_test_even_better_draw_circles()
+    run_test_even_better_draw_circles()
 
 
 # ----------------------------------------------------------------------
@@ -31,7 +31,7 @@ def run_test_draw_circles():
     """ Tests the   draw_circles   function. """
     # ------------------------------------------------------------------
     # Students:
-    #   Do NOT touch this function - it has no TODO in it.
+    #   Do NOT touch this function - it has no DONE in it.
     # ------------------------------------------------------------------
     print()
     print('--------------------------------------------------')
@@ -64,64 +64,116 @@ def draw_circles():
     window.close_on_mouse_click()
 
 # ----------------------------------------------------------------------
-# TODO: 2.
+# DONE: 2.
 #   First, RUN this program.  You will see that draw_circles draws
-#   concentric circles whose radii vary by 10.
+#   concentric circles whose radii vary by 10. DONE
 #
 #   A function that did the same thing as draw_circles, but allowed
 #   for the radii to vary by ANY desired amount would be MORE POWERFUL.
 #
 #   So, implement TWO functions immediately below this comment.
 #   They should be called:
-#      run_test_better_draw_circles
-#      better_draw_circles
+#      run_test_better_draw_circles DONE
+#      better_draw_circles DONE
 #
 #   Your   better_draw_circles  function should have a single PARAMETER
 #   that is the amount by which the radii of the circles increase.
 #   For example, if that parameter is given the value 10,
 #   then the circles have radii:  0  10  20  30  40 ... 200, respectively,
 #   just as in   draw_circles1.  But if that parameter is given the
-#   value 3, the circles have radii:  0  3  6  9  12  ... 60.
+#   value 3, the circles have radii:  0  3  6  9  12  ... 60. DONE
 #
 #   Your  run_test_better_draw_circles function should TEST your new
 #   better_draw_circles function, by calling it with different values
 #   for its argument.  Don't forget to put a call to
-#   run_test_better_draw_circles  in  main.
+#   run_test_better_draw_circles  in  main. DONE
 #
 #   You may find that COPY-AND-PASTE of the  draw_circles  and its
 #   run_test_draw_circles  may get you started more quickly on your new
 #   better_draw_circles  and  run_test_better_draw_circles.
 # ----------------------------------------------------------------------
 
+def run_test_better_draw_circles():
+
+
+    print()
+    print('--------------------------------------------------')
+    print('Testing  better_draw_circles:  See graphics window')
+    print('--------------------------------------------------')
+
+    #Test 1
+    better_draw_circles(10)
+
+
+def better_draw_circles(n):
+
+
+    window = rg.RoseWindow(800,800)
+    center = rg.Point(400,400)
+
+    for k in range(21):
+        circle = rg.Circle(center, n * k)
+        circle.attach_to(window)
+        window.render(0.05)
+
+    window.close_on_mouse_click()
+
 
 # ----------------------------------------------------------------------
-# TODO: 3.
+# DONE: 3.
 #   In the previous exercise, you made a MORE POWERFUL version
 #   of draw_circles by introducing a PARAMETER for the amount by
 #   which the radii of the concentric circles increase.
 #
 #   In this exercise, implement TWO MORE functions immediately below
 #   this comment. They should be called:
-#      run_test_even_better_draw_circles
-#      even_better_draw_circles
+#      run_test_even_better_draw_circles DONE
+#      even_better_draw_circles DONE
 #
 #   Your new   even_better_draw_circles  function should have
 #   SEVERAL parameters, for allowing the caller to vary what YOU
 #   choose to have the caller vary.  For example, you could have
 #   parameters for any or all of the following:
-#     -- The amount by which the radii vary (as you did above)
-#     -- The number of concentric circles drawn
+#     -- The amount by which the radii vary (as you did above) DONE
+#     -- The number of concentric circles drawn DONE
 #     -- The center of the concentric circles
 #     -- The outline_color of the concentric circles
-#     -- The speed at which the animation runs
+#     -- The speed at which the animation runs DONE
 #    and more.
 #
 #   A total of any THREE parameters (of your choosing) is enough,
-#   although you may have more.
+#   although you may have more. DONE
 #
 #   In testing your even_better_draw_circles function,
-#   can you make some fun pictures?
+#   can you make some fun pictures? DONE
 # ----------------------------------------------------------------------
+
+def run_test_even_better_draw_circles():
+    print()
+    print('--------------------------------------------------')
+    print('Testing  better_draw_circles:  See graphics window')
+    print('--------------------------------------------------')
+
+    # Test 1
+    even_better_draw_circles(8,40, 0.03)
+
+
+def even_better_draw_circles(radii,n,speed):
+
+    # radii = radius of circles
+    # n = number of circles
+    # speed = window rendering rate
+
+    window = rg.RoseWindow(800, 800)
+    center = rg.Point(400, 400)
+
+    for k in range(n):
+        circle = rg.Circle(center, radii * k)
+        circle.attach_to(window)
+        window.render(speed)
+
+    window.close_on_mouse_click()
+
 
 # ----------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
